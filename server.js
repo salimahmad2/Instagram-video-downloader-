c70f36d2512d7858ded8d92f027f433c2f67d819
salimahmad2 <salim.raza303030@gmail.com>
2025-12-19 01:02:10 +0600

    Refactor download route and streamline logging

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(compression());
 app.use(express.static('public'));
 
+// Safe download route
 app.get('/download', (req, res) => {
   const url = req.query.url;
   if (!url) return res.status(400).send('No URL provided');
@@ -13,24 +14,20 @@ app.get('/download', (req, res) => {
   res.setHeader('Content-Disposition', 'attachment; filename="instagram.mp4"');
   res.setHeader('Content-Type', 'video/mp4');
 
+  // small buffer to prevent crash on Render free plan
   const yt = spawn('yt-dlp', [
     '-f', 'bv*[ext=mp4]+ba[ext=m4a]/mp4',
     '--merge-output-format', 'mp4',
+    '--quiet',
     '-o', '-',
     url
   ]);
 
   yt.stdout.pipe(res);
 
-  yt.stderr.on('data', (data) => {
-    console.log(data.toString());
-  });
+  yt.stderr.on('data', (data) => console.log(data.toString()));
 
-  yt.on('close', () => {
-    res.end();
-  });
+  yt.on('close', () => res.end());
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
