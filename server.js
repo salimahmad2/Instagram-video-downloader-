9a96038c5c45b3afe43e2991d376f422225aac04
salimahmad2 <salim.raza303030@gmail.com>
2025-12-19 00:49:17 +0600

    Update server.js

--- server.js
+++ server.js
@@ -21,32 +21,14 @@ app.get('/download', (req, res) => {
   ]);
 
   yt.stdout.pipe(res);
-  yt.stderr.on('data', d => console.log(d.toString()));
-});
 
-app.listen(3000, () => {
-  console.log('Fast server running on http://localhost:3000');
-});
-```js
-const express = require('express');
-const { YTDlpWrap } = require('yt-dlp-wrap');
-const path = require('path');
+  yt.stderr.on('data', (data) => {
+    console.log(data.toString());
+  });
 
-const app = express();
-const ytDlp = new YTDlpWrap();
-
-app.use(express.static('public'));
-
-app.get('/download', async (req, res) => {
-  const url = req.query.url;
-  if (!url) return res.send('No URL provided');
-
-  res.setHeader('Content-Disposition', 'attachment; filename="instagram.mp4"');
-
-  ytDlp.execStream([
-    url,
-    '-f', 'mp4'
-  ]).pipe(res);
+  yt.on('close', () => {
+    res.end();
+  });
 });
 
 app.listen(3000, () => {
